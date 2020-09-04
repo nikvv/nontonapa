6f7b4d1187e3ca5b996106d5de528102a4bf1d14
NickoValentino <nicko.valentinoo@gmail.com>
2020-09-04 20:04:54 +0800

    Add Navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import fetch from './requests.js'
-import Row from './components/Row';
 import 'normalize.css'
+import Row from './components/Row';
 import Banner from './components/Banner.jsx';
 import Footer from './components/Footer.jsx';
+import Navbar from './components/Navbar.jsx'
 
 import './css/style.css'
 function App() {
   return (
     <div className="app">
-
+      <Navbar />
       <Banner fetchUrl={fetch.netflixOriginals} />
 
       <Row title='Netflix Originals' fetchUrl={fetch.netflixOriginals} />
